31ad9522e94e05f660220328d1828b2a1205f0bd
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-05-12 14:46:33 -0300

    Implemetação da camada de dominio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResTfulApiJavaBradescoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResTfulApiJavaBradescoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
